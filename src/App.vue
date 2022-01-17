b9a7cfeeab3ad4058be1a5c753ea8bdd66bced29
Tom Geitz <tgeitz@gmail.com>
2022-01-17 15:12:12 -0600

    Install vue-bootstrap

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,8 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
