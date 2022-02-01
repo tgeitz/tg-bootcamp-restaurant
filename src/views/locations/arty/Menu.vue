bd812ff702d0bd0ea159034d8ef182fb75509981
Arthur Lipka <lipkarth@my.dom.edu>
2022-01-31 22:13:58 -0600

    Create Menu.vue

---
+++ src/views/locations/arty/Menu.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Arty's Impossible Menu</h1>
+    <h3>Impossible Steak Combo</h3>
+    <ul>
+        <li>Plant Based Steak</li>
+        <li>Fries</li>
+        <li>Kale Juice</li>
+    </ul>
+</template>
